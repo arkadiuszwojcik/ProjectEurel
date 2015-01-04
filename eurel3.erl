aa2e1636db6ea2c2565c64cd08f18b2f47756811
arkadiuszwojcik <awojcik21@gmail.com>
2015-01-04 22:19:19 +0000

    formatting fix

--- eurel3.erl
+++ eurel3.erl
@@ -5,11 +5,11 @@
 prime_factors(Number, _N, Factors) when Number == 1 ->
 	Factors;
 prime_factors(Number, N, Factors) when Number rem N == 0 ->
-    prime_factors(Number div N, N, [N|Factors]);
+	prime_factors(Number div N, N, [N|Factors]);
 prime_factors(Number, N, Factors) ->
 	prime_factors(Number, N + 1, Factors).
 	
 main() ->
-    N = 600851475143,
+	N = 600851475143,
 	Factors = prime_factors(N, 2, []),
 	lists:max(Factors).
